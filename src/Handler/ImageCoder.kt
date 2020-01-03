bf0354eb877f70309b48000ad9cfa76e2a30befc
VadymMy <vadymmytrenko@ukr.net>
2020-01-03 23:41:59 +0200

    add firs implementation and test.

--- src/Handler/ImageCoder.kt
+++ src/Handler/ImageCoder.kt
@@ -2,7 +2,14 @@ package Handler
 import java.awt.image.*
 
 class ImageCoder(var mode: String = "enc", var password: String = "00000000") {
-    fun Code(image: BufferedImage){
-
+//    fun Code(image: BufferedImage): BufferedImage{
+//        var tmp = 1
+//        for(i in image.minX..image.width.minus(1)) {
+//            for (j in image.minY..image.height.minus(1)) {
+//                image.setRGB(i, j, tmp * image.getRGB(i, j).plus(password.toInt()))
+//                tmp *= -1
+//            }
+//        }
+//       return image
     }
 }
